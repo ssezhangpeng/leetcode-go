b6c6c1800d9ab9ddb51c52994a01eebad352509b
sweenzhang <sweenzhang@tencent.com>
2021-06-02 00:06:19 +0800

    add array files

---
+++ array/leetcode_0035/search_insert_position.go
@@ -0,0 +1,21 @@
+package leetcode_0035
+
+func searchInsert(nums []int, target int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	l := 0
+	r := len(nums) - 1
+	for l <= r {
+		mid := l + (r - l) >> 1
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target{
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+	return r + 1
+}
---
+++ array/leetcode_0977/squares_of_a_sort_array.go
@@ -0,0 +1,22 @@
+package leetcode_0977
+
+func sortedSquares(nums []int) []int {
+	length := len(nums)
+
+	l, r := 0, length-1
+	pos := length - 1
+	ans := make([]int, length)
+
+	for l <= r {
+		if nums[l] * nums[l] > nums[r] * nums[r] {
+			ans[pos] = nums[l] * nums[l]
+			l++
+		} else {
+			ans[pos] = nums[r] * nums[r]
+			r--
+		}
+		pos--
+	}
+
+	return ans
+}
