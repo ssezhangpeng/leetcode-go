5e0ab8d2b526eca2716fb0aafceddd4b1e228138
ssezhangpeng <ssezhangpeng@gmail.com>
2021-07-10 00:18:06 +0800

    update leetcode_0147

---
+++ 07_sort/leetcode_0021/merge_two_sorted_lists.go
@@ -0,0 +1,40 @@
+package leetcode_0021
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+
+	if l2 == nil {
+		return l1
+	}
+
+	var dummy ListNode
+	var pre = &dummy
+
+	for l1 != nil && l2 != nil {
+		if l1.Val <= l2.Val {
+			pre.Next = l1
+			l1 = l1.Next
+		} else {
+			pre.Next = l2
+			l2 = l2.Next
+		}
+		pre = pre.Next
+	}
+
+	if l1 != nil {
+		pre.Next = l1
+	}
+
+	if l2 != nil {
+		pre.Next = l2
+	}
+
+	return dummy.Next
+}
---
+++ 07_sort/leetcode_0147/insertion_sort_list.go
@@ -0,0 +1,13 @@
+package leetcode_0147
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func insertionSortList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	return nil
+}
