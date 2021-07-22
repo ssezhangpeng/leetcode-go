bbab62733699bae9e5e1d666263ba78ab5ad3cbd
ssezhangpeng <ssezhangpeng@gmail.com>
2021-07-23 00:45:23 +0800

    update leetcode_0085

--- 14_dp/leetcode_0085/maximal_rectangle.go
+++ 14_dp/leetcode_0085/maximal_rectangle.go
@@ -1,5 +1,28 @@
-package leetcode_0084
+package leetcode_0085
 
+func maximalRectangle(matrix [][]byte) int {
+	if len(matrix) == 0 {
+		return 0
+	}
+	ans := 0
+	m, n := len(matrix), len(matrix[0])
+
+	height := make([]int, n)
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if matrix[i][j] == '0' {
+				height[j] = 0
+			} else {
+				height[j] += 1
+			}
+		}
+		ans = max(ans, largestRectangleArea(height))
+	}
+
+	return ans
+}
+
+// leetcode_0084
 func largestRectangleArea(heights []int) int {
 	maxArea := 0
 	var stk []int
